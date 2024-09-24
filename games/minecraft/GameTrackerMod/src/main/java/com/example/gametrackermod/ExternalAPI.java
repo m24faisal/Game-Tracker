0dfd564d6b10900df69b51f39c02710cdd8f30c2
RaiyyanSiddiqui <raiyyan.siddiqui1@gmail.com>
2024-09-24 13:23:16 -0400

    forge mod updated - still crashes

--- games/minecraft/GameTrackerMod/src/main/java/com/example/gametrackermod/ExternalAPI.java
+++ games/minecraft/GameTrackerMod/src/main/java/com/example/gametrackermod/ExternalAPI.java
@@ -1,8 +1,8 @@
 package com.example.gametrackermod;
 
-//import com.rabbitmq.client.Channel;
-//import com.rabbitmq.client.Connection;
-//import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
 
@@ -10,10 +10,10 @@ import java.io.IOException;
 public class ExternalAPI {
     //rabbitmq connection variables
     private final String QUEUE_NAME = "examplequeue";
-    //public Channel channel = null;
+    public Channel channel = null;
 
     public ExternalAPI(){
-        /*
+
         //register rabbitmq connection
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
@@ -26,14 +26,16 @@ public class ExternalAPI {
         } catch (Exception e) {
             System.err.println("RabbitMQ setup failure");
         }
-         */
+
     }
 
     public void sendMessage(String message) throws IOException {
-        /*
+        if(channel == null || !channel.isOpen()){
+            throw new IOException("Channel null or not open");
+        }
         //send RabbitMQ message
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
         System.out.println(" [x] Sent '" + message + "'");
-         */
+
     }
 }
