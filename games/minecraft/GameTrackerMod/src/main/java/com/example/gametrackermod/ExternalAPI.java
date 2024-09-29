802c972d45bddf76eb5da6aa74ed072dccc25a9f
RaiyyanSiddiqui <raiyyan.siddiqui1@gmail.com>
2024-09-29 13:51:56 -0400

    testing without rabbitmq references

--- games/minecraft/GameTrackerMod/src/main/java/com/example/gametrackermod/ExternalAPI.java
+++ games/minecraft/GameTrackerMod/src/main/java/com/example/gametrackermod/ExternalAPI.java
@@ -1,41 +1,41 @@
 package com.example.gametrackermod;
-
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+//
+//import com.rabbitmq.client.Channel;
+//import com.rabbitmq.client.Connection;
+//import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
 
 
 public class ExternalAPI {
     //rabbitmq connection variables
-    private final String QUEUE_NAME = "examplequeue";
-    public Channel channel = null;
+//    private final String QUEUE_NAME = "examplequeue";
+//    public Channel channel = null;
 
     public ExternalAPI(){
-
-        //register rabbitmq connection
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        try (Connection connection = factory.newConnection()) {
-            this.channel = connection.createChannel();
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-            System.out.println(" [x] Sent '" + message + "'");
-        } catch (Exception e) {
-            System.err.println("RabbitMQ setup failure");
-        }
+//
+//        //register rabbitmq connection
+//        ConnectionFactory factory = new ConnectionFactory();
+//        factory.setHost("localhost");
+//        try (Connection connection = factory.newConnection()) {
+//            this.channel = connection.createChannel();
+//            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+//            String message = "Hello World!";
+//            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+//            System.out.println(" [x] Sent '" + message + "'");
+//        } catch (Exception e) {
+//            System.err.println("RabbitMQ setup failure");
+//        }
 
     }
 
     public void sendMessage(String message) throws IOException {
-        if(channel == null || !channel.isOpen()){
-            throw new IOException("Channel null or not open");
-        }
-        //send RabbitMQ message
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-        System.out.println(" [x] Sent '" + message + "'");
+//        if(channel == null || !channel.isOpen()){
+//            throw new IOException("Channel null or not open");
+//        }
+//        //send RabbitMQ message
+//        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+//        System.out.println(" [x] Sent '" + message + "'");
 
     }
 }
