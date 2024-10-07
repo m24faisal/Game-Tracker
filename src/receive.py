f1e37faf13f162dd41f4da17ba1a01ec71c79e38
m24faisal <mahirfaiz14@gmail.com>
2024-10-07 13:16:19 -0400

    Verify Received Data From Message Queue

--- src/receive.py
+++ src/receive.py
@@ -1,22 +1,24 @@
 import pika
 import pickle
 import dataFormat as df
+import json
 
 # Connect to RabbitMQ server
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 # Declare the same queue (in case it doesn’t exist)
-channel.queue_declare(queue='test_queue')
+channel.queue_declare(queue='data_gametracker')
 
 # Callback function to handle received messages
 def callback(ch, method, properties, body):
-    # Deserialize the data
-    data = pickle.loads(body)
-    print(f" [x] Received deserialized data: {data}")
+    # Read data from the received message queue
+    data = json.loads(body)
+    print(f"Received data: {body}")
+    print(data)
 
 # Set up the consumer to listen to the queue
-channel.basic_consume(queue='test_queue',
+channel.basic_consume(queue='data_gametracker',
                       on_message_callback=callback,
                       auto_ack=True)
 
